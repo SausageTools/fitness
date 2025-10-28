b2fcd2fe28a88cc0f346f37b1585094b2d309d62
Paul Park <sausagetools@gmail.com>
2025-10-28 08:08:41 +0000

    Changed title in Exercise Index

--- resources/js/Pages/Exercises/Index.vue
+++ resources/js/Pages/Exercises/Index.vue
@@ -20,7 +20,7 @@ const destroy = (id) => {
 </script>
 
 <template>
-    <Head title="Exercises" />
+    <Head title="Exercises, Yeah Baby!" />
 
     {{ exercises.links }}
     <Link :href="route('exercises.create')" class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 mt-4 mb-4">Add New Exercise</Link>
