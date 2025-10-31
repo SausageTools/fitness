909f0be625fd621a651243ef99111d7a0553f449
Paul Park <sausagetools@gmail.com>
2025-10-31 08:21:53 +0000

    web.php change

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,15 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    if (1 == 2) {
+
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
 });
 
 Route::resource('exercises', ExerciseController::class);
